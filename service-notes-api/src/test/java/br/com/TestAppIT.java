4a1af5f54a2dac9c9b3ebc02d715e9880a87a9a6
GeovaniTech <geovanidebastiani32@gmail.com>
2024-04-28 12:52:43 -0300

    feat:  Adding quarkus projeto to API

---
+++ service-notes-api/src/test/java/br/com/TestAppIT.java
@@ -0,0 +1,8 @@
+package br.com;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class TestAppIT extends TestAppTest {
+    // Execute the same tests but in packaged mode.
+}
