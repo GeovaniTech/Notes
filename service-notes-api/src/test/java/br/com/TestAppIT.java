57ad832e37890b43314d4f815b7442fa519876cc
Geovani Debastiani <geovanidebastiani32@gmail.com>
2024-11-24 14:56:44 -0300

    chore: Removing API files from project
    This application is going be more useful being just WEB.

--- service-notes-api/src/test/java/br/com/TestAppIT.java
+++
@@ -1,8 +0,0 @@
-package br.com;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class TestAppIT extends TestAppTest {
-    // Execute the same tests but in packaged mode.
-}
